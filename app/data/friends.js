f9e7f71c90fd153a39f5cf552d7fa1c673f76643
Angel Rivas <angel@prodigycode.com>
2019-08-10 05:14:36 -0500

    added api routes and js logic

---
+++ app/data/friends.js
@@ -0,0 +1,84 @@
+var friends = [
+    {
+      name: "Bad Bunny",
+      photo: "./images/Annie.png",
+      scores: [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+    },
+    {
+      name: "Bassnectar",
+      photo: "./images/jason.png",
+      scores: [
+        5,
+        5,
+        1,
+        1,
+        1,
+        5,
+        1,
+        1,
+        1,
+        5
+      ]
+    },
+    {
+      name: "Seven Lions",
+      photo: "./images/Freddie.jpg",
+      scores: [
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3
+      ]
+    },
+    {
+      name: "Dwayne 'The Rock' Johnson",
+      photo: "./images/Halloween.jpg",
+      scores: [
+        2,
+        3,
+        2,
+        1,
+        5,
+        4,
+        4,
+        2,
+        2,
+        3
+      ]
+    },
+    {
+      name: "John Cena",
+      photo: "./images/IMG_0252.jpg",
+      scores: [
+        1,
+        2,
+        1,
+        2,
+        2,
+        3,
+        1,
+        2,
+        1,
+        3
+      ]
+    },
+  ];
+  
+  module.exports = friends;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,35 @@
+var friends = require("../data/friends.js");
+
+module.exports = function (app) {
+
+    app.get("/api/friends", function (req, res) {
+        return res.json(friends);
+    });
+
+    app.post("/api/friends", function (req, res) {
+
+        // finds smallest number in array 
+        Array.min = function (array) {
+            return Math.min.apply(Math, array);
+        };
+
+        var newFriend = req.body;
+
+        // finds the difference between the new friend and the friends list
+        var matches = [];
+        const count = friends.length;
+        for (var i = 0; i < count; i++) {
+            var totalDifference = 0;
+            for (var j = 0; j < friends[i].scores.length; j++) {
+                totalDifference += Math.abs(newFriend.scores[j] - friends[i].scores[j]);
+            }
+            matches.push(totalDifference);
+        }
+        var smallestDiff = Array.min(matches);
+
+        var match = friends[matches.indexOf(smallestDiff)];
+
+        friends.push(newFriend);
+        res.json(match);
+    });
+};
